a1f9b41496f8576f82a005701ae5f6732534ea85
Kelsey <kelseyncook@gmail.com>
2024-06-01 15:22:02 -0500

    links-images/lemonchicken_files

--- links-images/lemonchicken_files/web.keywords.js
+++
@@ -1,5 +0,0 @@
-
-        window.$adManagementConfig = window.$adManagementConfig || {};
-        window.$adManagementConfig.web = window.$adManagementConfig.web || {};
-        window.$adManagementConfig.web.keywords = {"status":"PROCESSED","sales_kw":"oven baked lemon chicken,lemon chicken,chicken,fresh lemon juice,minute,salt,honey,pepper,side,butter,italian seasoning,sauce,chicken broth,chicken breast,medium-high","sales_cat":"210,211,214,216,217","sales_safety":"GGT0","sales_segment":"GumGum Verity Seasonal - Fathers Day,GumGum Verity Seasonal - Memorial Day,GumGum Verity Seasonal - Thanksgiving/Bl,GumGum Verity Seasonal - Christmas,GumGum Verity Seasonal - The Big Game (A"};
-      
